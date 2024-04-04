2c649064eb8999dedc4f2ba7bb84cf09c31439e2
eug <18398188+EuHello@users.noreply.github.com>
2024-04-04 11:51:17 +0800

    Update bash script with filecheck for peptalk.sh, and proper array arguments for python script

--- add_job.sh
+++ add_job.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # add_job.sh
 
-source peptalk.sh
+intro="./peptalk.sh"
+if [ -e "$intro" ]; then
+  source peptalk.sh
+fi
 
 echo "Enter jobs applied in this format: date(blank=today or yyyy-mm-dd); job title; company name"
-read inputs
+read -r inputs
 echo "$inputs" >> "data.csv"
 
-cmd1="maintain.py -m"
-echo "Running $cmd1"
-python3 $cmd1
+cmd1=("maintain.py" "-m")
+echo "Running ${cmd1[*]}"
+python3 "${cmd1[@]}"
